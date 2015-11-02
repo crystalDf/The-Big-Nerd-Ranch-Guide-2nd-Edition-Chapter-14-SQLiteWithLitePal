cdbf3895e67d8d9a992be4d8270a5cdc3bb81de5
crystalDf <chendong333@gmail.com>
2015-11-03 01:07:59 +0800

    Chapter 14 SQLiteWithLitePal

---
+++ app/src/main/java/com/star/sqlitewithlitepal/MainActivity.java
@@ -0,0 +1,18 @@
+package com.star.sqlitewithlitepal;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.litepal.tablemanager.Connector;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SQLiteDatabase sqLiteDatabase = Connector.getDatabase();
+    }
+}
---
+++ app/src/main/java/com/star/sqlitewithlitepal/model/News.java
@@ -0,0 +1,53 @@
+package com.star.sqlitewithlitepal.model;
+
+
+import java.util.Date;
+
+public class News {
+
+    private int id;
+    private String title;
+    private String content;
+    private Date publishedDate;
+    private int commentCount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+}
