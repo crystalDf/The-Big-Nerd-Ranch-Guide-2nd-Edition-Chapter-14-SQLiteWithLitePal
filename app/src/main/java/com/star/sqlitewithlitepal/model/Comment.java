dc379774c05c976ef952fb23cc6f1f717ea25b7e
crystalDf <chendong333@gmail.com>
2015-11-04 01:17:04 +0800

    Chapter 14 SQLiteWithLitePal onUpgrade()

---
+++ app/src/main/java/com/star/sqlitewithlitepal/model/Comment.java
@@ -0,0 +1,35 @@
+package com.star.sqlitewithlitepal.model;
+
+
+import java.util.Date;
+
+public class Comment {
+
+    private int id;
+    private String content;
+    private Date publishedDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+}
